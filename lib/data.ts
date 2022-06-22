6f8645594f00c4ddbab6fdf42ac46a34518c4987
Ayomide Oguntuase <oguntuase3@gmail.com>
2022-06-22 23:03:54 +0100

    updated story.json for me

--- lib/data.ts
+++ lib/data.ts
@@ -6,10 +6,10 @@ export default {
   oss,
 } as const;
 
-export type Application = typeof story.applications[0];
+// export type Application = typeof story.applications[0];
 export type Experience = typeof story.experiences[0];
-export type Artifact = typeof story.artifacts[0];
-export type OpenSource = typeof oss[keyof typeof oss] & {
+// export type Artifact = typeof story.artifacts[0];
+// export type OpenSource = typeof oss[keyof typeof oss] & {
   first_commit_date?: string;
   last_commit_date?: string;
 };
